d283641b4fdba6aceea94eba5755b9253547b4e9
esrakbc <122485941+esrakbc@users.noreply.github.com>
2023-03-12 20:30:56 +0300

    Update Recap1.java

--- src/test/java/Recap1.java
+++ src/test/java/Recap1.java
@@ -2,6 +2,8 @@ public class Recap1 {
     public static void main(String[] args) {
         System.out.println("merhaba");
         System.out.println("dünya");
+        
+        //merhaba dünya
     }
 }
 
