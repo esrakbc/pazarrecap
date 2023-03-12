efd6e3240f3db142ea538456fce2052f7384cfa4
esrakbc <ekabaca09@gmail.com>
2023-03-12 20:53:47 +0300

    v8

--- src/test/java/Recap1.java
+++ src/test/java/Recap1.java
@@ -2,7 +2,7 @@ public class Recap1 {
     public static void main(String[] args) {
         System.out.println("merhaba");
         System.out.println("dünya");
-        
+        System.out.println("b129");
         //merhaba dünya
     }
 }
